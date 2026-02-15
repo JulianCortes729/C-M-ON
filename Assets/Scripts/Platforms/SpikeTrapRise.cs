56e9f57a8637348cb251d7f3d292bb1ed5bbc157
Julian Cortes <juliancortes729@gmail.com>
2026-02-15 15:21:26 -0300

    ADD animaciones y sonidos de advertencia/descenso de pinchos
    Introduje los clips de animación SpikesWarning y SpikesLower y agregué múltiples assets de sonido para los pinchos (activación, desactivación y advertencia).
    
    Actualicé Spikes.controller agregando nuevos estados, transiciones y parámetros (IsActivated / Warn), e integré los estados de advertencia y descenso en la máquina de estados; además ajusté las transiciones de SpikesRise.
    
    Modifiqué los valores de tiempo y posición del clip SpikesRise.
    
    Actualicé prefabs: asigné el tag Trampoline y corregí su rotación; modifiqué el prefab TrapSpikeTroll (eliminé el componente BoxCollider y agregué los campos timer y boxCollider).
    
    También actualicé la entrada de malla en la escena Level1 y ProjectSettings/TagManager para reflejar estos cambios.
    
    Estos cambios implementan las animaciones de advertencia y descenso de la trampa de pinchos, junto con sus ajustes de audio, escena y prefabs asociados.

--- Assets/Scripts/Platforms/SpikeTrapRise.cs
+++ Assets/Scripts/Platforms/SpikeTrapRise.cs
@@ -1,16 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpikeTrapRise : MonoBehaviour
 {
-    public Animator spikesAnimator;
+    private bool isActivated = false;
+
+    [Header("Configuracion de tiempos")]
+    [SerializeField] private float activeDuration;
+    [SerializeField] private float riseDelay;
+    private WaitForSeconds waitTimer;
+    private WaitForSeconds waitDelay;
+
+    
+
+    [Header("Referencias Visuales y Físicas")]
+    [SerializeField] private BoxCollider damageCollider;
+    [SerializeField] private Animator spikesAnimator;
+
+
+    [Header("Audio Keys")]
+    [SerializeField] private string warnAudioKey = "TrapWarn";
+    [SerializeField] private string attackAudioKey = "TrapAttack";
+    [SerializeField] private string retractAudioKey = "TrapRetract";
+
+    void Start()
+    {
+        waitTimer = new WaitForSeconds(activeDuration);
+        waitDelay = new WaitForSeconds(riseDelay);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        TryActivateTrap(other);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        TryActivateTrap(other);
+    }
+    
+    void TryActivateTrap(Collider other)
+    {
+        if (other.CompareTag("Player") && !isActivated)
         {
-            spikesAnimator.SetTrigger("Activate");
+            StartCoroutine(ActivateSpikes());
         }
+    }
+
+    IEnumerator ActivateSpikes()
+    {
+        isActivated = true;
+        spikesAnimator.SetTrigger("Warn");
+        MultiAudioPool.Instance?.Play(warnAudioKey, transform.position);
+
+        yield return waitDelay;
+
+        spikesAnimator.SetBool("IsActivated", isActivated);
+        MultiAudioPool.Instance?.Play(attackAudioKey, transform.position);
+        damageCollider.enabled = true;
+
+       
+
+        yield return waitTimer;
+       
+        damageCollider.enabled = false;
+        isActivated = false;
+       
+        spikesAnimator.SetBool("IsActivated", isActivated);
+        MultiAudioPool.Instance?.Play(retractAudioKey, transform.position);
+
+
+
     }
 }
